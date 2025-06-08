f6d699345d66cbe471688c12a39f6b3f983c6dab
Abdullahi Ismail Ali <abdullahiismailali04@gmail.com>
2025-06-08 14:58:51 +0300

    Updated Script.js
    Added a function to hide the navigation menu when clicked on any navigation link.

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ const navbarLinks = document.querySelector('.navbar-links');
 toggleButton.addEventListener('click', () => {
   navbarLinks.classList.toggle('active');
 });
+
+// HIDE NAVIGATION LINKS AFTER CLICKING ON A LINK
+const navLinks = document.querySelectorAll('.navbar-links a');
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    navbarLinks.classList.remove('active');
+  });
+});
